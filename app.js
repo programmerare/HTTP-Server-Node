9f643b0a15f842ab6f218c6b6c3bf8d47a2c366f
Marcel Wendler <marcel.wendler008@gmail.com>
2024-05-10 16:40:44 +0200

    included modules, created http server, server listens on port 3000

---
+++ app.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const express = require("express");
+const http = require("http");
+const io = require("socket.io");
+
+const PORT = 3000;
+
+let app = express();
+http.createServer(app);
+io(http);
+
+app.listen(PORT, function(req, res){
+    console.log(`Listening on port ${PORT}`);
+})
