cd81b4733895c21308682af9b474681edddd5a2e
Marcel Wendler <marcel.wendler008@gmail.com>
2024-05-12 13:13:29 +0200

    changed variable names

--- public/js/form_validate.js
+++ public/js/form_validate.js
@@ -5,12 +5,12 @@ let form_ref = document.querySelector("form");
 form_ref.addEventListener("submit", validate_form);
 
 function validate_form(e){
-    let email_ref = this.querySelector("input[type=text]");
+    let username_ref = this.querySelector("input[type=text]");
     let password_ref = this.querySelector("input[type=password]");
 
     try{
-        if(email_ref.value.length === 0)
-            throw {"elemref": email_ref, "message": "Provide a username!"};
+        if(username_ref.value.length === 0)
+            throw {"elemref": username_ref, "message": "Provide a username!"};
         if(password_ref.value.length < 10)
             throw {"elemref": password_ref, "message": "Password must be at least 10 characters long!"};
     }
