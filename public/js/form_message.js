7db5352759c0e681d52888b904e8577168d2e63f
Marcel Wendler <marcel.wendler008@gmail.com>
2024-05-14 16:53:26 +0200

    changes to the DOM are now handled by the server

--- public/js/form_message.js
+++ public/js/form_message.js
@@ -4,12 +4,15 @@ let form_ref = document.querySelector("form");
 
 form_ref.addEventListener("submit", send_message);
 
+// send message to server
 function send_message(e){
     let message_ref = this.querySelector("input[type=text]");
 
     try{
         if(message_ref.value.length === 0)
             throw { "elemref": message_ref, "message": "Message cannot be empty!" };
+        if(this.querySelector("#error").firstChild)
+            this.querySelector("#error").textContent = "";
     }
     catch(oError){
         e.preventDefault();
